b34d2d266a6478663e58b8e5aeaf3c8b64acd81e
Shivam <aryansn0101@gmail.comgit config --global user.name Your>
2021-06-04 11:10:21 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import "./App.css";
+import ModalImage from "react-modal-image";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+
+
+  const [gallery_list, set_gallery_list] = useState([]);
+  const [loading, set_loading] = useState(true);
+
+  useEffect(() => {
+    
+    fetch("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=fc7218c544eefe8c010a66f0253717a7&per_page=&format=json&nojsoncallback=1&api_sig=6accc064702d74d4f98c0d46d8914dfc")
+    .then(data=>data.json())
+    .then(doc=>{
+
+
+      console.log(doc);
+
+      set_gallery_list(doc.photos.photo);
+
+
+      set_loading(false);
+
+
+
+    })
+
+
+  }, [])
+
+
+
+
+if(loading){
+  return(
+
+    <p>Loading</p>
+
+  )
 }
 
-export default App;
+
+
+  return (
+
+
+    <div className="app">
+
+    <div className="gallery_grid">
+
+
+        {
+
+          gallery_list.map((item,index)=>{
+            return(
+
+               
+
+                 <div className="gallery_comp">
+                   <ModalImage
+                      className="image"
+                      small={ "https://live.staticflickr.com/"+ item.server  + "/" + item.id + "_"+item.secret +"_w"+ ".jpg"}
+                      large={ "https://live.staticflickr.com/"+ item.server  + "/" + item.id + "_"+item.secret +"_w"+ ".jpg"}
+                      alt={item.title}
+                    />;
+                 </div>
+
+            )
+          })
+
+        }
+
+
+  
+    </div>
+
+    </div>
+  )
+}
+
+export default App
